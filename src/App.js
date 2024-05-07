09016ea939392333f766af675f06b2e40d2ce281
dsm122n <dsm122n@gmail.com>
2024-05-07 01:38:50 -0400

    iniciando el editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Editor from '@monaco-editor/react';
+import { marked } from 'marked'; // library to convert markdown to HTML
 
-function App() {
+export default function App() {
+  
+  const [contenido, setContenido] = useState('');
+
+  const handleEditorChange = (value, event) => {
+    setContenido(marked(value)); // convert markdown to HTML and update state
+  }
+  const onMount = (editor, monaco) => {
+    editor.focus();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex'}}>
+      <Editor
+        height="100vh"
+        width="50vw"
+        theme='vs-dark'
+        defaultLanguage='markdown'
+        onMount={onMount}
+        onChange={handleEditorChange}
+      />
+      {/* Preview of Editor markdown */}
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '50vw', backgroundColor: 'black'}}>
+        <div style={{height: '100%', width: '100%', backgroundColor: 'white', padding: '20px'}}>
+          <div dangerouslySetInnerHTML={{__html: contenido}}></div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
