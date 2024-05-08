8306c8a60e1eb56e54b58a6e0ef8122c69e3742a
dsm122n <dsm122n@gmail.com>
2024-05-08 00:58:46 -0400

    editor con autocompletado mejorado

--- src/components/snippets.js
+++ src/components/snippets.js
@@ -1,6 +1,91 @@
 import * as monaco from 'monaco-editor'; // library to use monaco editor
 
 const snippets = {
+    // Diagnósticos obste
+    'dg_ingreso_egreso': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+            '## Diagnósticos ingreso:',
+            '1. ${1:Multípara de} ${2}, ${3:edad} años',
+            '2. Embarazo de ${4} semanas por FUR${5}',
+            '3. Trabajo de parto fase ${6:activa}',
+            '4. IMC ${7}',
+            '5. Riesgo tromboembólico ${8}',
+            '',
+            '## Diagnósticos egreso:',
+            '1. ${9:Multípara de} ${10}, ${3:edad} años',
+            '2. Puérpera ${11:2} días ${12:parto vaginal/cesárea}',
+
+        ].join('\n'),
+        detail: 'Diagnósticos ingreso y egreso'
+    },
+
+    // Procedimientos
+    'parto_vaginal': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+            '- Parto vaginal espontáneo (${1:fecha} a las ${2:hora}), sin complicaciones. PN: ${3} g, Talla ${4} cm, APGAR ${5}-${6}',
+        ].join('\n'),
+        detail: 'Parto PTVE'
+    },
+    'cesarea': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+            '- Cesárea (${1:fecha} a las ${2:hora}), sin complicaciones. PN: ${3} g, Talla ${4} cm, APGAR ${5}-${6}',
+        ].join('\n'),
+        detail: 'Cesárea'
+    },
+
+    // examenes
+    'ex_hemograma': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'Hb ${1:num}, Leucocitos ${2:num}, Plaquetas ${3:num}',
+        description: 'Examen hemograma',
+        detail: 'Hemograma'
+    },
+    'ex_coagulacion': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'INR ${1:num}, TP ${2:num}%, TTPK ${3:num}s',
+        description: 'Examen coagulación',
+        detail: 'Coagulación'
+    },
+    'ex_hepatico': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'GOT ${1:num}, GPT ${2:num}, BT ${3:num}, BD ${4:num}, GGT ${5:num}, FA ${6:num}',
+        description: 'Examen hepático',
+        detail: 'Hepático'
+    },
+    'ex_función_renal': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'Creatinina ${1:num}, BUN ${2:num}',
+        description: 'Examen función renal',
+        detail: 'Función renal'
+    },
+
+    //Resumen
+    'resumen_puerpera': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+            'Usuaria de ${1:edad} años, ${2:multípara}, cursando embarazo de ${3:num} semanas por FUR. Consulta al servicio de urgencias por contracciones uterinas dolorosas ${4:cantidad y otros síntomas}. Al examen físico se constata dilatación cervical ${5:num}cm, borramiento ${6:num}%, centrado.',
+            'Se decide ingreso por ${7:trabajo de parto en fase activa}.',
+            '',
+            'Evoluciona ${8:evolución}. Se asiste parto vaginal, finalizando embarazo el ${8:fecha} a las ${9:hora} horas, sin complicaciones. Se recibe recién nacido de sexo ${10:sexo}, Peso ${11:PN} g, Talla ${12:TN} cm, APGAR ${13}-${14}. En puerperio evoluciona con buen manejo del dolor, con loquios hemáticos normales, logrando lactancia materna sin dificultades. Dada evolución favorable, se decide dar alta.',
+        ].join('\n'),
+        detail: 'Resumen puerpera'
+    },
+
+    // combo medicamentos partos 
+    'combo_fco_parto': {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+        '- Paracetamol ${1:1 gr} cada ${2:8 horas} ${3:ev}',
+        '- Ketoprofeno ${4:100 mg} cada ${5:8 horas} ${6:ev}',
+        '- Ampicilina según protocolo de profilaxis sepsis neonatal',
+        '- Anestesia ${7:peridural}',
+        '- Oxitocina según protocolo${8}',
+        ].join('\n'),
+        detail: 'Combo fármacos parto'
+    },
 
     // Fármacos
     'paracetamol_vo': {
